b0a686a2f3c6648135508dfb45b259689999d4c3
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-08-25 14:38:47 +0900

    stage 1 : 현재시각, date picker 이용한 선택시각 표시

--- alarm_practice/ViewController.swift
+++ alarm_practice/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let timeSelector:Selector = #selector(ViewController.updateTime)
+    let interval = 0.1
+    var count = 0
+    @IBOutlet weak var lblCurrentTime: UILabel!
+    @IBOutlet weak var lblPickerTime: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
-
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender;
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblPickerTime.text = "선택시각 : " + formatter.string(from: datePickerView.date)
+    }
+    @objc func updateTime(){
+        let date = NSDate()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시각 : " + formatter.string(from: date as Date)
+    }
 }
 
