1a9801e79d93f9880e86dd88b32971b9cbd43db6
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-08-25 14:53:51 +0900

    stage 2 : 시간 되면 배경색 변경

--- alarm_practice/ViewController.swift
+++ alarm_practice/ViewController.swift
@@ -8,11 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     let timeSelector:Selector = #selector(ViewController.updateTime)
     let interval = 0.1
     var count = 0
+    
     @IBOutlet weak var lblCurrentTime: UILabel!
     @IBOutlet weak var lblPickerTime: UILabel!
+    
+    var pickerTime = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,12 +29,25 @@ class ViewController: UIViewController {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
         lblPickerTime.text = "선택시각 : " + formatter.string(from: datePickerView.date)
+        
+        let alarmformatter = DateFormatter()
+        alarmformatter.dateFormat = "hh:mm aaa"
+        pickerTime = alarmformatter.string(from: datePickerView.date)
+        
     }
     @objc func updateTime(){
         let date = NSDate()
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
         lblCurrentTime.text = "현재시각 : " + formatter.string(from: date as Date)
+        
+        let alarmformatter = DateFormatter()
+        alarmformatter.dateFormat = "hh:mm aaa"
+        if (alarmformatter.string(from: date as Date) == pickerTime){
+            view.backgroundColor = UIColor.orange
+        } else {
+            view.backgroundColor = UIColor.yellow
+        }
     }
 }
 
